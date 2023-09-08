39741a1685afbddc1a75cf7134e8889fa5da958e
Diana Smilian <dianka.kulakevich@gmail.com>
2023-09-08 12:00:56 +0300

    hide password

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+import e from 'express';
+import mongoose from 'mongoose';
 import app from './app.js';
 
-app.listen(3000, () => {
-  console.log('Server running. Use our API on port: 3000');
-});
+// const DB_HOST = 'mongodb+srv://diankasmilian:20diDidi01@cluster.ou6lwrg.mongodb.net/db-contacts?retryWrites=true&w=majority'
+
+mongoose.connect(procces.env.DB_HOST)
+.then(() => {
+  app.listen(3000, () => {
+    console.log('Server running. Use our API on port: 3000');
+  });
+})
+.catch((error) => {
+console.log(error.message)
+process.exit(1)
+})
+
+
